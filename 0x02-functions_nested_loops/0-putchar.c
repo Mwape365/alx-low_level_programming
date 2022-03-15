5abd6f9a8e0ce779d6007113dfac6d1d2505fe2f
mwape365 <unratedideas@gmail.com>
2022-03-15 15:36:38 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints "putchar"
+ * Return: 0
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
