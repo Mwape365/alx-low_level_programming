48606f9fcfa49eb734713a76fe4f8180e591510c
mwape365 <unratedideas@gmail.com>
2022-03-24 09:09:08 -0700

    Task 4 I am a kind of paranoid in reverse.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
 
 /**
  * reverse_array - Function reverses the content of an array of integers
- * @: Array of integers
+ * @a: Array of integers
  * @n: Number of elements of the array
  *
  * Return: 0
