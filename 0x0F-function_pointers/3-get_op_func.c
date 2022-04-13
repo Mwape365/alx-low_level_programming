86585dcdce92833299981c5f6b6d2562d53da73c
mwape365 <unratedideas@gmail.com>
2022-04-13 13:08:29 -0700

    Task 3 A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * get_op_func - function selects the correct function
+ * to peerform the operation asked by the user
+ * @s: op str
+ *
+ * Return: 0
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (op_s[i].op)
+	{
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
+		i++;
+	}
+	return (NULL);
+}
