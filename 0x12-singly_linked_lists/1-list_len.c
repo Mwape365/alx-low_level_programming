272ec345ff1aaf001327cdf3e852be3605079a59
mwape365 <unratedideas@gmail.com>
2022-04-21 10:27:35 -0700

    Task 1 List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - Function returns the number of elements
+ * in a linked list_t list
+ * @h: pointer to the list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t cont = 0;
+
+	while (h)
+	{
+		cont++;
+		h = h->next;
+	}
+
+	return (cont);
+}
