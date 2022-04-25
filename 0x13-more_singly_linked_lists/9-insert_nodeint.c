0a60f15d5612bbb33a9c4ce70cab7fe06fdb134c
mwape365 <unratedideas@gmail.com>
2022-04-25 05:57:57 -0700

    Task 9 Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,22 +1,43 @@
 #include "lists.h"
 
 /**
- * sum_listint - Function returns sum of all data in list
- * @head: points to list
+ * insert_nodeint_at_index - Function inserts new node at given position
+ * @head: ptr to ptr to list
+ * @idx: position in list to insert
+ * @n: int to put in new node
  *
- * Return: sum or 0
+ * Return: pointer to new node or NULL
  */
 
-int sum_listint(listint_t *head)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int sum = 0;
-
+	/* declarations */
+	listint_t *location;
+	listint_t *new;
+	unsigned int i;
+	/* check for NULL */
 	if (!head)
-		return (0);
-	while (head)
+		return (NULL);
+	location = *head;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
 	{
-		sum += head->n;
-		head = head->next;
+		new->next = location;
+		*head = new;
+		return (*head);
 	}
-	return (sum);
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (location)
+			location = location->next;
+		else
+			return (NULL);
+	}
+
+	new->next = location->next;
+	location->next = new;
+	return (new);
 }
