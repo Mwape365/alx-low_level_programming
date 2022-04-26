acad5bfe96862b0a84a1ebcde605d258a5fb5ed4
mwape365 <unratedideas@gmail.com>
2022-04-26 15:01:30 -0700

    Task 0 Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_listint - Function that prints all the elements of
- * a listint_t list.
+ * a listint_t list
  *
  * @h: Parameter Pointer to the head of the linked list
  *
  * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
